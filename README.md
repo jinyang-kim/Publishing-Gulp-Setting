8fef87d6df2972db6ed372b307e362fb528b90d7
jinyang_kim <jinyang_kim@daekyo.com>
2022-04-07 18:37:30 +0900

    feat : gulp 1차 세팅 완료
     - gulp build, dev, deploy 작업 처리 완료
     - README.md 업데이트
     - 기능 업데이트

--- README.md
+++ README.md
@@ -3,22 +3,28 @@
 Gulp 환경의 Publishing 작업을 하기 위해 만들어진 Repository 입니다.
 - Gulp에서 JavaScript를 사용하기 위해 gulp파일을 babel을 이용해 컴파일합니다.
 
-Gulp JS Site (URL) : [https://gulpjs.com/](https://gulpjs.com/)
+* Gulp JS Site (URL) : [https://gulpjs.com/](https://gulpjs.com/)
+* Gulp Packages Searching : https://www.npmjs.com/
+* CSS 네이밍 규칙(BEM / Block__Element--Modifier) : https://nykim.work/15
 
 **1. 필수 설치 (required)**
 -
 1) node.js와 npm이 먼저 미리 설치되어 있어야 한다.
- - [https://nodejs.org/ko/](https://nodejs.org/ko/) 페이지에서 LTS 버전으로 설치
- - node 설치 확인
-   - node -v
-   - npm -v
+ * [https://nodejs.org/ko/](https://nodejs.org/ko/) 페이지에서 LTS 버전으로 설치
+ * node 설치 확인
+   * node -v
+   * npm -v
 2) gulp 설치
- - npm version : npm install gulp-cli -g
- - yarn version : yarn add gulp-cli
- - gulp 설치 확인 : gulp -v
+ * npm version : npm install gulp-cli -g
+ * yarn version : yarn add gulp-cli
+ * gulp 설치 확인 : gulp -v
 3) gulp 실행 명령어
- - gulp : gulp task_name;
- - yarn : yarn task_name;
+ * gulp : gulp task_name;
+ * yarn : yarn task_name;
+4) 프로젝트 불러왔을 경우 (필수)
+ * 프로젝트를 불러왔을 경우, node_module이 설치되지 않았기 때문에 Gulp 프로젝트가 실행되지 않습니다.
+ * 프로젝트 저장 시 node_modules, Compile된 파일들은 저장소에 들어가지 않아도 되는 불필요한 파일이기 때문에 .gitignore 파일에서 제외시켜둡니다.
+ * Terminal에 npm install 명령어를 입력하면 package.json에 들어있는 정보를 읽어서 프로젝트에 필요한 node_module들을 설치합니다.
 
 **2. 프로젝트 경로 (Directory)**
 - 
@@ -35,3 +41,7 @@ Gulp JS Site (URL) : [https://gulpjs.com/](https://gulpjs.com/)
     * gulpfile.babel.js (Gulp JavaScript 관리)
     * package.json (node package 관리)
     * README.md
+
+**3. 파일 추가 관련**
+- 
+* SCSS파일에서 언더스코어(_)를 붙이면 SASS한테 자기들을 Compile하지말고 사용만 하라고 알려주는것
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -2,9 +2,16 @@
 import gulp from "gulp";
 import gpug from "gulp-pug";
 import del from "del";
-import ws from "gulp-webserver";
-import image from "gulp-imagemin";
-// import sass from "gulp-sass";
+import gwebserver from "gulp-webserver";
+import gimagemin from "gulp-imagemin";
+import autoprefixer from "gulp-autoprefixer";
+import csso from "gulp-csso";
+import sourcemaps from "gulp-sourcemaps";
+import bro from "gulp-bro";
+import babelify from "babelify";
+import uglify from "gulp-uglify";
+import plumber from "gulp-plumber";
+import ghPages from "gulp-gh-pages";
 
 const sass = require('gulp-sass')(require('sass'));
 
@@ -16,6 +23,7 @@ const routes = {
 		dest: "build"
 	},
 	html: {
+		watch: "src/html/**/*.html",
 		src: "src/html/*",
 		dest: "build/html"
 	},
@@ -32,51 +40,89 @@ const routes = {
 		src: "build/css"
 	},
 	js: {
-		src: "src/js/*",
+		watch: "src/js/**/*.js",
+		src: "src/js/main.js",
 		dest: "build/js"
 	}
+	// js: {
+	// 	warch: "src/js/**/*.js",
+	// 	src: "src/js/*",
+	// 	dest: "build/js"
+	// }
 }
 
 /* Gulp Tasks */
 const pug = () =>
 	gulp
 		.src(routes.pug.src)
+		.pipe(plumber())
 		.pipe(gpug())
 		.pipe(gulp.dest(routes.pug.dest));
 
-const clean = () => del(["build"]);
+const clean = () => del(["build", ".publish"]);
 
 const webserver = () => 
 	gulp
 		.src("build")
-		.pipe(ws({ 
+		.pipe(plumber())
+		.pipe(gwebserver({ 
+			// port: 8080,
 			livereload : true, 
 			open: true 
 		}));
 
+const gh = () => 
+	gulp
+		.src("build/**/*")
+		.pipe(ghPages());
+
 const watch = () => {
 	gulp.watch(routes.pug.watch, pug);
 	gulp.watch(routes.scss.watch, styles);
+	gulp.watch(routes.js.watch, js);
 	// gulp.watch(routes.img.src, img);
 }
+
+const js = () => 
+	gulp	
+		.src(routes.js.src)
+		.pipe(plumber())
+		.pipe(bro({
+			transform: [
+				// babelify.configure({ presets: ["es2015"] }),
+				// babelify.configure({ presets: ["@babel/preset-env"] }),
+				// [ 'uglifyify', { global: true } ]
+				babelify.configure({ presets: ["@babel/preset-env"] })
+			]
+		}))
+		.pipe(uglify())
+		.pipe(gulp.dest(routes.js.dest));
 
 const img = () => 
 	gulp
 		.src(routes.img.src)
-		.pipe(image())
+		.pipe(plumber())
+		.pipe(gimagemin())
 		.pipe(gulp.dest(routes.img.dest));
 
 const styles = () => 
 	gulp
 		.src(routes.scss.src)
+		.pipe(plumber())
+		.pipe(sourcemaps.init())
 		.pipe(sass().on('error', sass.logError))
+		.pipe(autoprefixer())
+		.pipe(csso())
+		.pipe(sourcemaps.write("./"))
 		.pipe(gulp.dest(routes.scss.dest));
 
 /* Gulp Builds */
 const prepare = gulp.series([clean, img]);
 
-const assets = gulp.series([pug, styles]);
+const assets = gulp.series([pug, styles, js]);
 
-const postDev = gulp.parallel([webserver, watch]);
+const live = gulp.parallel([webserver, watch]);
 
-export const dev = gulp.series([prepare, assets, postDev]);
+export const build = gulp.series([prepare, assets]);
+export const dev = gulp.series([build, live]);
+export const deploy = gulp.series([build, gh, clean]);
